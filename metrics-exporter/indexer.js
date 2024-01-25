b7b8b96688b5f656a3f4296ddd67a1610ec2fe4d
clemensgg <csc@soundportal.at>
2024-01-25 16:20:27 +0100

    Refactor: move db definition to dedicated file

--- metrics-exporter/indexer.js
+++ metrics-exporter/indexer.js
@@ -1,20 +1,18 @@
 import axios from 'axios';
-import sqlite3 from 'sqlite3';
 import config from './config.js';
+import { 
+  saveTransactionData,
+  saveTotalMetricsData,
+  saveGranteeMisbehaviorData,
+  saveLastBlockData,
+  getLatestBlockHeightFromDB
+} from './db.js'
 
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const { Tx } = require('cosmjs-types/cosmos/tx/v1beta1/tx.js');
 const { PubKey } = require('cosmjs-types/cosmos/crypto/secp256k1/keys.js');
 const { pubkeyToAddress } = require('@cosmjs/amino');
-
-const sqlite = sqlite3.verbose();
-const db = new sqlite.Database('./relayerMetrics.db', (err) => {
-  if (err) {
-    console.error('[ERR] ' + err.message);
-  }
-  console.log('[INFO] Connected to the relayer metrics SQLite database.');
-});
 
 let latestBlockHeight = 0;
 let latestBlockTime = "";
@@ -23,148 +21,6 @@ let totalGasWanted = 0;
 let totalGasUsed = 0;
 let totalFee = 0;
 let transactionCount = 0;
-
-db.serialize(() => {
-  db.run(`CREATE TABLE IF NOT EXISTS relayer_transactions (
-    block_height INTEGER,
-    block_time TEXT,
-    relayer_address TEXT,
-    msg_array TXT,
-    gas_wanted INTEGER,
-    gas_used INTEGER,
-    fee_amount INTEGER,
-    gas_price REAL
-  )`);
-
-  db.run(`CREATE TABLE IF NOT EXISTS total_metrics (
-    block_height INTEGER,
-    block_time TEXT,
-    total_gas_wanted INTEGER,
-    total_gas_used INTEGER,
-    total_fee INTEGER,
-    transaction_count INTEGER
-  )`);
-
-  db.run(`CREATE TABLE IF NOT EXISTS last_block (
-    block_height INTEGER,
-    block_time TEXT
-  )`);
-
-  db.run(`CREATE TABLE IF NOT EXISTS grantee_misbehaviors (
-    block_height INTEGER,
-    block_time TEXT,
-    grantee_address TEXT,
-    msg_array TEXT
-  )`);
-});
-
-async function saveTransactionData(blockHeight, blockTime, relayerAddress, msgArray, gasWanted, gasUsed, feeAmount, gasPrice) {
-  db.run(`INSERT INTO relayer_transactions (
-    block_height, 
-    block_time, 
-    relayer_address, 
-    msg_array, 
-    gas_wanted, 
-    gas_used, 
-    fee_amount, 
-    gas_price
-    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`, 
-    [
-      blockHeight, 
-      blockTime, 
-      relayerAddress, 
-      msgArray, 
-      gasWanted, 
-      gasUsed, 
-      feeAmount, 
-      gasPrice
-    ], (err) => {
-      if (err) {
-        console.error('[ERR] ' + err.message);
-      }
-  });
-  console.log(`[DEBUG] saved relayer_transaction for: ${relayerAddress}`);
-  return;
-}
-
-async function saveTotalMetricsData(blockHeight, blockTime, totalGasWanted, totalGasUsed, totalFee, transactionCount) {
-  db.run(`INSERT INTO total_metrics (
-    block_height, 
-    block_time, 
-    total_gas_wanted, 
-    total_gas_used, 
-    total_fee, 
-    transaction_count
-    ) VALUES (?, ?, ?, ?, ?, ?)`, 
-    [
-      blockHeight, 
-      blockTime, 
-      totalGasWanted, 
-      totalGasUsed, 
-      totalFee, 
-      transactionCount
-    ], (err) => {
-      if (err) {
-        console.error('[ERR] ' + err.message);
-      }
-  });
-  console.log(`[DEBUG] saved total_metrics.`);
-  return;
-}
-
-async function saveLastBlockData(blockHeight, blockTime) {
-  db.run(`INSERT INTO last_block (
-    block_height, 
-    block_time
-    ) VALUES (?, ?)`, 
-    [
-      blockHeight, 
-      blockTime
-    ], (err) => {
-      if (err) {
-        console.error('[ERR] ' + err.message);
-      }
-  });
-  return;
-}
-
-async function saveGranteeMisbehaviorData(blockHeight, blockTime, granteeAddress, typeString) {
-  db.run(`INSERT INTO grantee_misbehaviors (
-    block_height, 
-    block_time, 
-    grantee_address, 
-    msg_array
-    ) VALUES (?, ?, ?, ?)`, 
-    [
-      blockHeight, 
-      blockTime, 
-      granteeAddress, 
-      typeString
-    ], (err) => {
-      if (err) {
-        console.error('[ERR] ' + err.message);
-      }
-  });
-  return;
-}
-
-async function getLatestBlockHeightFromDB() {
-  try {
-    const row = await new Promise((resolve, reject) => {
-      db.get("SELECT MAX(block_height) as max_height FROM last_block", (err, row) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(row);
-        }
-      });
-    });
-    return row ? row.max_height : config.indexer_start_block_height;
-  } catch (error) {
-    console.error('[ERR] Error fetching latest block height from database:', error);
-    return false;
-  }
-}
 
 async function getBlock(height) {
   try {
@@ -330,12 +186,3 @@ export async function indexer() {
     }
   }, config.indexer_poll_frequency); 
 }
-
-process.on('exit', () => {
-  db.close((err) => {
-    if (err) {
-      console.error('[ERR] ' + err.message);
-    }
-    console.log('[INFO] Close the database write connection.');
-  });
-});
